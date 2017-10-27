584926d24f9c45fa2962874cadeb4f7e9d0784ae
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-27 14:35:29 +0100

    Add Attribute Option implementation and tests

--- src/AttributeOption.php
+++ src/AttributeOption.php
@@ -1,10 +1,46 @@
 <?php
-
-
+declare(strict_types=1);
 namespace SnowIO\Magento2DataModel;
 
-
-class AttributeOption
+final class AttributeOption
 {
+    public static function of(string $attributeCode, string $value, string $label): self
+    {
 
+    }
+
+    public function getAttributeCode(): string
+    {
+
+    }
+
+    public function getValue(): string
+    {
+
+    }
+
+    public function getLabel(): string
+    {
+
+    }
+
+    public function withLabel(): self
+    {
+
+    }
+
+    public function toJson(): array
+    {
+
+    }
+
+    public function equals($attributeOption): bool
+    {
+
+    }
+
+    private function __construct()
+    {
+
+    }
 }
--- test/unit/AttributeOptionTest.php
+++ test/unit/AttributeOptionTest.php
@@ -1,7 +1,44 @@
 <?php
+declare(strict_types=1);
 
 
-class AttributeOptionTest
+use PHPUnit\Framework\TestCase;
+use SnowIO\Magento2DataModel\AttributeData;
+use SnowIO\Magento2DataModel\AttributeOption;
+
+class AttributeOptionTest extends TestCase
 {
+    public function testToJson()
+    {
+        $attributeOption = AttributeOption::of('size', 'large', 'Large');
+        self::assertEquals([
+            'entity_type' => 4,
+            'attribute_code' => 'size',
+            'option' => [
+                'value' => 'large',
+                'label' => 'Large',
+            ]
+        ], $attributeOption->toJson());
+    }
 
+    public function testWithers()
+    {
+        $attributeOption = AttributeOption::of('size', 'large', 'Large')
+            ->withLabel('Larger');
+        self::assertEquals('size', $attributeOption->getAttributeCode());
+        self::assertEquals('large', $attributeOption->getValue());
+        self::assertEquals('Large', $attributeOption->getLabel());
+    }
+
+    public function testEquals()
+    {
+        $attributeOption = AttributeOption::of('size', 'large', 'Large')
+            ->withLabel('Larger');
+        self::assertTrue($attributeOption->equals(AttributeOption::of('size', 'large', 'Large')
+            ->withLabel('Larger')));
+        self::assertFalse($attributeOption->equals(AttributeOption::of('size', 'small', 'Small')));
+        self::assertFalse($attributeOption->equals(AttributeOption::of('size', 'large', 'Large')
+            ->withLabel('Largest')));
+        self::assertFalse($attributeOption->equals(AttributeData::of('size', 'text', 'Size')));
+    }
 }
