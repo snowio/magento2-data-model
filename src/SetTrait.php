112fc0c19f99c9c830b6aa6d42a139459ad79204
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-31 13:17:41 +0000

    Update set trait

--- src/SetTrait.php
+++ src/SetTrait.php
@@ -1,9 +1,12 @@
 <?php
 declare(strict_types=1);
 namespace SnowIO\Magento2DataModel;
-
 trait SetTrait
 {
+
+    /**
+     * @return static
+     */
     public static function of(array $items): self
     {
         $set = new self;
@@ -21,11 +24,50 @@ trait SetTrait
         return $set;
     }
 
+    /**
+     * @return static
+     */
     public static function create(): self
     {
         return new self;
     }
 
+    /**
+     * @return static
+     */
+    public function filter(callable $predicate): self
+    {
+        $result = new self;
+        $result->items = \array_filter($this->items, $predicate);
+        return $result;
+    }
+
+    /**
+     * @return static
+     */
+    public function map(callable $fn): self
+    {
+        $items = \array_map($fn, $this->items);
+        return self::of($items);
+    }
+
+    /**
+     * @return static
+     */
+    public function flatMap(callable $fn): self
+    {
+        $mappedItems = [];
+        foreach ($this->items as $item) {
+            foreach ($fn($item) as $mappedItem) {
+                $mappedItems[] = $mappedItem;
+            }
+        }
+        return self::of($mappedItems);
+    }
+
+    /**
+     * @return static
+     */
     public function add(self $otherSet): self
     {
         if ($otherSet->overlaps($this)) {
@@ -33,6 +75,31 @@ trait SetTrait
         }
         $result = new self;
         $result->items = \array_merge($this->items, $otherSet->items);
+        return $result;
+    }
+
+    /**
+     * @return static
+     */
+    public function merge(self $otherSet): self
+    {
+        $result = new self;
+        $result->items = \array_merge($this->items, $otherSet->items);
+        return $result;
+    }
+
+    /**
+     * @return static
+     */
+    public function diff(self $otherSet): self
+    {
+        $result = new self;
+        foreach ($this->items as $key => $item) {
+            $otherItem = $otherSet->items[$key] ?? null;
+            if (!isset($otherItem) || !self::itemsAreEqual($item, $otherItem)) {
+                $result[$key] = $item;
+            }
+        }
         return $result;
     }
 
@@ -66,18 +133,15 @@ trait SetTrait
         if (!$object instanceof self) {
             return false;
         }
-
         if (\count($this->items) != \count($object->items)) {
             return false;
         }
-
         foreach ($this->items as $key => $item) {
-            $otherItem = $object->items[$key];
-            if (!self::itemsAreEqual($item, $otherItem)) {
+            $otherItem = $object->items[$key] ?? null;
+            if ($otherItem === null || !self::itemsAreEqual($item, $otherItem)) {
                 return false;
             }
         }
-
         return true;
     }
 
@@ -92,7 +156,6 @@ trait SetTrait
 
     private function __construct()
     {
-
     }
 
     private static function itemsAreEqual($item1, $item2): bool
