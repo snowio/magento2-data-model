38b1c675516d4c3cf396cf443ca241eaa35f6063
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-31 08:16:29 +0000

    Add support AttributeSetCode: Commit also includes examples of other Extension attributes in the tests

---
+++ src/AttributeSetCode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SnowIO\Magento2DataModel;
+
+final class AttributeSetCode extends ExtensionAttribute
+{
+    public static function of(string $value): self
+    {
+        return new self($value);
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function equals($attributeSetCode): bool
+    {
+        return parent::equals($attributeSetCode) &&
+            ($attributeSetCode->value === $this->value);
+    }
+
+    protected function getValueJson()
+    {
+        return $this->value;
+    }
+
+    private $value;
+
+    protected function __construct(string $value)
+    {
+        parent::__construct('attribute_set_code');
+        $this->value = $value;
+    }
+}
