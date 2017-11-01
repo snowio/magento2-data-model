30d56b910fa203bae9317f779e2586b73bfeefc8
alexwanyoike <wanyoike.alexander@gmail.com>
2017-11-01 10:37:11 +0000

    Make final

--- src/AttributeDataSet.php
+++ src/AttributeDataSet.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace SnowIO\Magento2DataModel;
 
-class AttributeDataSet implements \IteratorAggregate
+final class AttributeDataSet implements \IteratorAggregate
 {
     use SetTrait;
 
---
+++ test/unit/Command/CommandTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace SnowIO\Magento2DataModel\Test;
+
+
+class CommandTest
+{
+
+}
