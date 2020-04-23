0a56276c449b4d473c014e0b581b8a3c071eeb8a
Nei Santos <nrauni@gmail.com>
2020-04-23 02:23:23 -0300

    Add option for swatch attribute

--- test/unit/AttributeDataTest.php
+++ test/unit/AttributeDataTest.php
@@ -8,6 +8,7 @@ use SnowIO\Magento2DataModel\AttributeData;
 use SnowIO\Magento2DataModel\AttributeScope;
 use SnowIO\Magento2DataModel\CustomAttribute;
 use SnowIO\Magento2DataModel\FrontendInput;
+use SnowIO\Magento2DataModel\SwatchInputType;
 
 class AttributeDataTest extends TestCase
 {
@@ -23,19 +24,35 @@ class AttributeDataTest extends TestCase
         ], $attribute->toJson());
     }
 
+    public function testToJsonWithSwatchInputType()
+    {
+        $attribute = AttributeData::of('diameter', FrontendInput::TEXT, 'Diameter')
+            ->withSwatchInputType(SwatchInputType::DROPDOWN);
+        self::assertEquals([
+            'attribute_code' => 'diameter',
+            'is_required' => false,
+            'frontend_input' => 'text',
+            'default_frontend_label' => 'Diameter',
+            'scope' => AttributeScope::GLOBAL,
+            'swatch_input_type' => SwatchInputType::DROPDOWN,
+        ], $attribute->toJson());
+    }
+
     public function testWithers()
     {
         $attribute = AttributeData::of('diameter', FrontendInput::BOOLEAN, 'Diameter')
             ->withRequired(true)
             ->withFrontendInput(FrontendInput::TEXT)
             ->withScope(AttributeScope::GLOBAL)
-            ->withDefaultFrontendLabel('Diameter Updated!!!');
+            ->withDefaultFrontendLabel('Diameter Updated!!!')
+            ->withSwatchInputType(SwatchInputType::TEXT);
 
         self::assertEquals('diameter', $attribute->getCode());
         self::assertEquals(true, $attribute->isRequired());
         self::assertEquals(FrontendInput::TEXT, $attribute->getFrontendInput());
         self::assertEquals('global', $attribute->getScope());
         self::assertEquals('Diameter Updated!!!', $attribute->getDefaultFrontendLabel());
+        self::assertEquals(SwatchInputType::TEXT, $attribute->getSwatchInputType());
     }
 
     /**
@@ -47,11 +64,29 @@ class AttributeDataTest extends TestCase
             ->withScope('site');
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidSwatchInputType()
+    {
+        AttributeData::of('diameter', FrontendInput::BOOLEAN, 'Diameter')
+            ->withSwatchInputType('bla');
+    }
+
     public function testEquals()
     {
         $attribute = AttributeData::of('diameter', FrontendInput::TEXT, 'Diameter');
         self::assertTrue($attribute->equals(AttributeData::of('diameter', FrontendInput::TEXT, 'Diameter')));
         self::assertFalse($attribute->equals(AttributeData::of('viscosity', FrontendInput::TEXT, 'Viscosity')));
         self::assertFalse($attribute->equals(CustomAttribute::of('diameter', '300')));
+    }
+
+    public function testEqualsWithSwatchInputType()
+    {
+        $attribute = AttributeData::of('diameter', FrontendInput::TEXT, 'Diameter')
+            ->withSwatchInputType(SwatchInputType::TEXT);
+        self::assertTrue($attribute->equals(AttributeData::of('diameter', FrontendInput::TEXT, 'Diameter')
+            ->withSwatchInputType(SwatchInputType::TEXT)));
+        self::assertFalse($attribute->equals(AttributeData::of('viscosity', FrontendInput::TEXT, 'Viscosity')));
     }
 }
