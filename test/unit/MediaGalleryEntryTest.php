1fc4b4bb531c69d1639f10a17a67fd367baac82c
Nei Santos <nrauni@gmail.com>
2025-03-20 10:02:22 -0300

    Add extension attributes to media gallery

--- test/unit/MediaGalleryEntryTest.php
+++ test/unit/MediaGalleryEntryTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace  SnowIO\Magento2DataModel\Test;
 
 use PHPUnit\Framework\TestCase;
+use SnowIO\Magento2DataModel\ExtensionAttribute;
+use SnowIO\Magento2DataModel\ExtensionAttributeSet;
 use SnowIO\Magento2DataModel\MediaGalleryEntry;
 
 class MediaGalleryEntryTest extends TestCase
@@ -60,10 +62,30 @@ class MediaGalleryEntryTest extends TestCase
             ->withFile('path/image.jpg')
             ->withTypes(['image', 'small_image', 'thumbnail'])
             ->withDisabled(true)
-            ->withPosition(1);
+            ->withPosition(1)
+            ->withExtensionAttributes(
+                ExtensionAttributeSet::of([
+                    ExtensionAttribute::of('video_content', [
+                        "media_type" => 'external-video',
+                        "video_provider" => 'youtube',
+                        "video_url" => 'https://www.youtube.com/watch?v=fake',
+                        "video_title" => 'Video title',
+                        "video_description" => 'Video description',
+                        "video_metadata" => '',
+                    ])
+                ])
+            );
 
         $this->assertEquals(1, $mediaGallery->getDisabled());
         $this->assertEquals(1, $mediaGallery->getPosition());
+        $this->assertEquals(['video_content'=> [
+            'media_type' => 'external-video',
+            'video_provider' => 'youtube',
+            'video_url' => 'https://www.youtube.com/watch?v=fake',
+            'video_title' => 'Video title',
+            'video_description' => 'Video description',
+            'video_metadata' => '',
+        ]], $mediaGallery->getExtensionAttributes()->toJson());
         $this->assertEquals('path/image.jpg', $mediaGallery->getFile());
         $this->assertEquals(['image', 'small_image', 'thumbnail'], $mediaGallery->getTypes());
     }
